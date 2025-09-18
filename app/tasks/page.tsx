6ab6f6c7bfce66257bd1580a8dda62d2e70c8841
Patrícia <pg.costa@campus.fct.unl.pt>
2025-09-18 23:26:18 +0100

    fetch tasks

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,3 +1,23 @@
-export default function Tasks() {
-  return <h2>This is the tasks page.</h2>;
+type Task = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
+
+export default async function Tasks() {
+  const data = await fetch("http://localhost:3008/api/tasks/ ");
+  const posts: Task[] = await data.json();
+
+  return (
+    <>
+      <h2>Tasks:</h2>
+      <ul>
+        {posts.map((post) => (
+          <li className={post.completed ? "line-through" : ""} key={post.id}>
+            {post.text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
